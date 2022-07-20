1323e15c563c2676fe01307ce8249418466c87e5
Michael Marshall <388-s1786208@users.noreply.git.ecdf.ed.ac.uk>
2022-07-20 20:21:12 +0100

    Plot scripts updated. Produced M1 and F1M documents with these versions

--- Scripts/Plot/PlotTwoPointFit.sh
+++ Scripts/Plot/PlotTwoPointFit.sh
@@ -6,6 +6,10 @@ PCMakeScriptOutDir
 echo "$Ensemble: Two point fits"
 
 InDir=$PlotData/fit/2ptp2/Summary
+echo InDir=$InDir
+
+#set -x
+shopt -s nullglob
 
 # Optional environment variables
 # nonew:  Set to anything to disable new plots
@@ -18,8 +22,16 @@ WhereClauses[eq]='column("ti") == column("ti1")'
 WhereClauses[lt]='column("ti") <  column("ti1")'
 WhereClauses[gt]='column("ti") >  column("ti1")'
 extralbl='column("ti") == column("ti1") ? "" : "(".stringcolumn("ti1").")"'
+# stats command in Gnuplot 5.4 always throws an error
+# fixed in gnuplot 5.5, but that's not released yet.
+# This is my workaround. Don't like it because I use AWK numbered fields, not names
+declare -A GotDataClauses
+GotDataClauses[eq]='$2 == $4'
+GotDataClauses[lt]='$2 <  $4'
+GotDataClauses[gt]='$2 >  $4'
 
 for f in $InDir/*.params_sort.*.txt
+#for f in $InDir/h447_h447_p2_[45]*.params_sort.*.txt
 do
   Dir=${f%/*}
   Filename=${f##*/}
@@ -42,9 +54,10 @@ do
       do
         for WC in "${!WhereClauses[@]}"
         do
+          NumRows=$(awk "/^[-0-9]/ {if(${GotDataClauses[$WC]}) {z++;exit}}; END {print 0+z}" < $f)
           #sort='-V -k 5r -k 1'
           series=$series save="${series}_$WC" where="${WhereClauses[$WC]}" \
-            extralbl="$extralbl" sort=$sort PlotFitRes.sh $f
+            extralbl="$extralbl" sort=$sort GotData=$NumRows PlotFitRes.sh $f
         done
       done
     fi
