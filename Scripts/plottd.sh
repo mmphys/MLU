f4c759f7dd496b778c51b8fbfd9457b8364bbb42
Michael Marshall <388-s1786208@users.noreply.git.ecdf.ed.ac.uk>
2022-10-18 19:51:40 +0100

    plottd.sh: Can now plot points outside fit range

--- Scripts/plottd.sh
+++ Scripts/plottd.sh
@@ -3,6 +3,9 @@
 
 ###################################################
 # Plot theory - data from fit
+#
+# TODO: Fits with a single point don't show fit error range (filledcurves)
+#
 ###################################################
 
 PlotFunction()
@@ -12,26 +15,25 @@ gnuplot <<-EOFMark
 #Command-line options
 PlotFile="$PlotFile"
 my_yrange="${yrange:-*:*}"
-FieldNames="${fields:-log}"
-FieldNamesText="${fieldtext:-effective mass}"
+FieldName="${field:-log}"
+FieldNameText="${fieldtext:-effective mass}"
 MyTitle="${title}"
 PDFSize="${size:-6in,3in}"
 Save="${save}"
 xAxis="${x:-t}"
 ModelMin=${mmin:-0}
 ModelMax=${mmax:--1}
+OriginalTI="${ti}"
+OriginalTF="${tf}"
+OriginalFiles="${files}"
+DoLog=0${log+1}
 
-##################################################
-# TODO: Convert to command-line parameters
-
-# These are the files data points outside the fit range come from
-OriginalNames='../F1M/corr/3pt_sp2/quark_l_h385_gT_dt_16_p2_0_ps2_0_g5P_g5P.fold.1835672416.h5 ../F1M/corr/3pt_sp2/quark_l_h385_gT_dt_20_p2_0_ps2_0_g5P_g5P.fold.1835672416.h5 ../F1M/corr/3pt_sp2/quark_l_h385_gT_dt_24_p2_0_ps2_0_g5P_g5P.fold.1835672416.h5 ../F1M/corr/3pt_sp2/quark_l_h385_gT_dt_28_p2_0_ps2_0_g5P_g5P.fold.1835672416.h5 ../F1M/corr/3pt_sp2/quark_l_h385_gT_dt_32_p2_0_ps2_0_g5P_g5P.fold.1835672416.h5'
-OriginalTI='2 2 2 2 2'
-OriginalTF='14 18 22 26 30'
-
-##################################################
-
-NumFields=words(FieldNames)
+xOffset=FieldName eq "log" ? -0.5 : 0.0
+NumTI=words( OriginalTI )
+NumTF=words( OriginalTF )
+#print 'NumTI='.NumTI.', OriginalTI='.OriginalTI
+#print 'NumTF='.NumTF.', OriginalTF='.OriginalTF
+#print OriginalFiles
 
 if( Save ne "" ) {
   if( PDFSize ne "" ) { PDFSize='size '.PDFSize }
@@ -41,9 +43,6 @@ if( Save ne "" ) {
 
 set key bottom right
 
-fld=1
-FieldName=word(FieldNames,fld)
-
 # Get number of models
 if( ModelMax < 0 ) {
   stats PlotFile using 'model' nooutput
@@ -52,44 +51,67 @@ if( ModelMax < 0 ) {
 NumModels=ModelMax - ModelMin + 1
 if( NumModels < 1 ) { print "Error: ".NumModels." models"; exit 1 }
 
+# This function sets the X-Axis to the requested field
+GetUsingX(col)='using (stringcolumn("field") eq FieldName && column("model")==(model-1+ModelMin) ? column('.col.') : NaN )'
+
 # Work out ranges for each file
 array FitTMin[NumModels]
 array FitTMax[NumModels]
 array FitSeqMin[NumModels]
 array FitSeqMax[NumModels]
+array PlotTMin[NumModels]
+array PlotTMax[NumModels]
+array PlotTWidth[NumModels]
+TotalPlotTWidth=0
+UsingX=GetUsingX("'seq'")
 do for [model=1:NumModels] {
-  stats PlotFile using (stringcolumn('field') eq FieldName && \
-                        column('model') == (model-1+ModelMin) ? column('seq') : NaN ) \
-                        : 't' nooutput
+  stats PlotFile @UsingX : 't' nooutput
   FitSeqMin[model]=floor(STATS_min_x)
   FitSeqMax[model]=floor(STATS_max_x)
   FitTMin[model]=STATS_min_y
   FitTMax[model]=STATS_max_y
   #print "model=".(model-1+ModelMin).": seq=[".FitSeqMin[model].",".FitSeqMax[model]."], t=[".sprintf("%g",FitTMin[model]).",".sprintf("%g",FitTMax[model])."]"
+  PlotTMin[model]=(model <= NumTI) ? ( word(OriginalTI,model)+0 ) : ( FitTMin[model] - 0.5 )
+  PlotTMax[model]=(model <= NumTF) ? ( word(OriginalTF,model)+0 ) : ( FitTMax[model] + 0.5 )
+  #print 'PlotTMin[model]='.sprintf("%g",PlotTMin[model])
+  #print 'PlotTMax[model]='.sprintf("%g",PlotTMax[model])
+  PlotTWidth[model]=PlotTMax[model]-PlotTMin[model]
+  #print 'PlotTWidth[model]='.sprintf("%g",PlotTWidth[model])
+  TotalPlotTWidth=TotalPlotTWidth+PlotTWidth[model]
 }
-
 set yrange [@my_yrange]
 #set xrange [8.3:10.7]
 #set xrange [7.8:11.2]
-#set logscale y
+if( DoLog ) { set logscale y }
 
 # Work out layout
 LMar=0.075
 RMar=0.025
 PlotWidth=1-LMar-RMar
 
-# Total number of data points
-NumDataPoints=0
-do for [model=1:NumModels] { NumDataPoints=NumDataPoints+FitSeqMax[model] - FitSeqMin[model] + 1 }
-
 array SubPlotWidth[NumModels]
 array SubPlotLeft[NumModels]
 array SubPlotRight[NumModels]
 do for [model=1:NumModels] {
-  SubPlotWidth[model]=PlotWidth * ( FitSeqMax[model] - FitSeqMin[model] + 1 ) / NumDataPoints
+  SubPlotWidth[model]=PlotWidth * PlotTWidth[model] / TotalPlotTWidth
   SubPlotLeft[model]=model == 1 ? LMar : SubPlotRight[model - 1]
   SubPlotRight[model]=SubPlotLeft[model]+SubPlotWidth[model]
 }
+
+PointSizeData="0.6"
+PointSizeTheory="0.72"
+UsingX=GetUsingX("xAxis")
+
+PlotPrefix='plot PlotFile '.UsingX
+PlotPrefix=PlotPrefix.':(column("theory_low")):(column("theory_high"))'
+PlotPrefix=PlotPrefix.' with filledcurves notitle lc "skyblue" fs transparent solid 0.5'
+PlotPrefix=PlotPrefix.', "" '.UsingX
+PlotPrefix=PlotPrefix.': (column("theory"))'
+PlotPrefix=PlotPrefix.'with linespoints notitle lc "blue" dt 5 pt 4 ps '.PointSizeTheory
+
+PlotSuffix=', PlotFile '.UsingX
+PlotSuffix=PlotSuffix.': (column("data")):(column("data_low")):(column("data_high"))'
+PlotSuffix=PlotSuffix.' with yerrorbars notitle lc "red" pt 13 ps '.PointSizeData
 
 set multiplot layout 1, NumModels
 
@@ -100,24 +122,32 @@ if( MyTitle ne "" ) { set title word(MyTitle,model) }
 set lmargin at screen SubPlotLeft[model]
 set rmargin at screen SubPlotRight[model]
 
-set xtics ceil(FitTMin[model]), 2, floor(FitTMax[model])
+set xtics ceil(PlotTMin[model]), 2, floor(PlotTMax[model])
 
 if( model == 2 ) { set format y ''; unset ylabel }
 
-plot \
-  PlotFile using (stringcolumn('field') eq word(FieldNames,fld) && \
-                  column('model')==(model-1+ModelMin) ? column(xAxis) \
-                  : NaN ) \
-    :(column('theory_low')):(column('theory_high')) \
-      with filledcurves notitle lc "skyblue" fs transparent solid 0.5\
-  ,'' using (stringcolumn('field') eq word(FieldNames,fld) && \
-              column('model')==(model-1+ModelMin) ? column(xAxis) : NaN ) \
-          : (column('theory')) \
-      with linespoints notitle lc "blue" dt 5 pt 4\
-  ,'' using (stringcolumn('field') eq word(FieldNames,fld) && \
-              column('model')==(model-1+ModelMin) ? column(xAxis) : NaN ) \
-          : (column('data')):(column('data_low')):(column('data_high')) \
-    with yerrorbars notitle lc "red" pt 13
+ThisFile=word( OriginalFiles, model )
+if( ThisFile eq '' ) {
+  ExtraPlot=''
+} else {
+  OtherFieldName=(FieldName eq "log") ? "exp" : FieldName
+  ColumnX="(column('t')+(".sprintf("%g",xOffset)."))"
+  #print 'ColumnX='.ColumnX
+  ConditionA=ColumnX.'>='.sprintf("%g",PlotTMin[model]).' && '.ColumnX.'<'.sprintf("%g",FitTMin[model])
+  #print 'ConditionA='.ConditionA
+  ConditionB=ColumnX.'<='.sprintf("%g",PlotTMax[model]).' && '.ColumnX.'>'.sprintf("%g",FitTMax[model])
+  #print 'ConditionB='.ConditionB
+  Condition='(('.ConditionA.') || ('.ConditionB.'))'
+  #print 'Condition='.Condition
+  ExtraPlot=', "'.ThisFile.'" using ('.Condition.' ? '.ColumnX.' : NaN )'
+  ExtraPlot=ExtraPlot.": (column('".OtherFieldName."'))"
+  ExtraPlot=ExtraPlot.": (column('".OtherFieldName."_low'))"
+  ExtraPlot=ExtraPlot.": (column('".OtherFieldName."_high'))"
+  ExtraPlot=ExtraPlot." with yerrorbars notitle lc 'black' pt 13 ps ".PointSizeData
+  #print 'ExtraPlot='.ExtraPlot
+}
+
+eval PlotPrefix.ExtraPlot.PlotSuffix
 }
 
 unset multiplot
