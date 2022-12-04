dd96ae0668d35f6d9439d3e53076b4dee07bcfe1
Michael Marshall <388-s1786208@users.noreply.git.ecdf.ed.ac.uk>
2022-12-04 14:07:35 +0000

    F1M: First 2pt and R3 fit selections. Matrix elements gamma T and gamma spatial

--- Scripts/FitMELF1M.sh
+++ Scripts/FitMELF1M.sh
@@ -18,6 +18,10 @@ MesonFit[h385_s,0]=corr_6_29_5_29
 MesonFit[s_l,0]=corr_10_26_7_26 # Original with Peter
 MesonFit[s_l,1]=corr_8_28_9_28
 MesonFit[s_l,2]=corr_8_26_8_28
+MesonFit[s_l,3]=corr_8_28_8_28
+MesonFit[s_l,4]=corr_10_28_8_28
+MesonFit[s_l,5]=corr_10_28_12_28
+MesonFit[s_l,6]=corr_10_28_12_28
 
 ############################################################
 
@@ -55,6 +59,7 @@ Gamma=gT
 #YRangeR3Raw=(0.00068:0.000782 0.00060:0.00070 0.00056:0.00066 0.00052:0.00062)
 #YRangeR3=(0.8:1.07 0.75:0.97 0.75:0.92 0.65:0.92)
 
+if [ "${DoAll+x}" = x ]; then
 NumExp=2 DeltaT="24 28 32" TI='13 14 14' TF='16 20 24' yrangeR3=0.00068:0.000782 yrangeMEL=0.527:0.537 DoFit 0
 
 NumExp=2 DeltaT="28 32" TI='12 12' TF='20 24' yrangeR3=0.00060:0.00070 yrangeMEL=0.486:0.496 DoFit 1 # Alternate
@@ -64,3 +69,16 @@ NumExp=2 DeltaT="24 28 32" TI='14 15 15' TF='17 20 24' yrangeR3=0.000185:0.00022
 
 NumExp=2 DeltaT="24 28 32" TI='12 13 13' TF='15 19 23' yrangeR3=0.00056:0.00066 yrangeMEL=0.452:0.462 DoFit 2
 NumExp=2 DeltaT="24 28 32" TI='13 13 14' TF='16 20 24' yrangeR3=0.000155:0.000195 yrangeMEL=0.44:0.467 Gamma=gXYZ DoFit 2
+
+NumExp=3 DeltaT="24 28 32" TI='11 12 13' TF='14 18 22' yrangeR3=0.000540:0.000625 yrangeMEL=0.42:0.44 DoFit 3
+NumExp=2 DeltaT="24 28 32" TI='12 13 14' TF='16 20 24' yrangeR3=0.000130:0.000170 yrangeMEL=0.41:0.44 Gamma=gXYZ DoFit 3
+
+NumExp=2 DeltaT="24 28 32" TI='11 12 13' TF='14 18 22' yrangeR3=0.0005:0.00057 yrangeMEL=0.395:0.415 DoFit 4
+NumExp=2 DeltaT="24 28" TI='12 13 14' TF='16 20 24' yrangeR3=0.000115:0.00014 yrangeMEL=0.38:0.41 Gamma=gXYZ DoFit 4
+
+NumExp=3 DeltaT="24 28 32" TI='11 12 13' TF='14 18 22' yrangeR3=0.00045:0.0006 yrangeMEL=0.36:0.4 DoFit 5
+NumExp=2 DeltaT="24 28" TI='12 14 14' TF='16 20 24' yrangeR3=0.00011:0.00014 yrangeMEL=0.34:0.4 Gamma=gXYZ DoFit 5
+
+NumExp=2 DeltaT="24 28 32" TI='12 15 18' TF='15 19 22' yrangeR3=0.00044:0.00052 yrangeMEL=0.32:0.37 DoFit 6
+NumExp=2 DeltaT="24 28 32" TI='10 12 16' TF='16 20 24' yrangeR3=0.00008:0.00014 yrangeMEL=0.27:0.37 Gamma=gXYZ DoFit 6
+fi
--- Scripts/FitTwoPointF1M.sh
+++ Scripts/FitTwoPointF1M.sh
@@ -6,10 +6,19 @@
 set -e
 export Ensemble=F1M
 
+if [ "${DoAll+x}" = x ]; then
+
 # D_s
 p=0 TI=6 TF=29 NumExp=3 TI2=5 Meson=h385_s yrange='0.72:0.735' FitTwoPoint.sh
 
-# These are the best two Kaon fits, working with Peter Friday 25 Nov 2022
-#p=0 TI=10 TF=26 NumExp=2 TI2=7 Meson=s_l yrange='0.187:0.197' FitTwoPoint.sh
-#p=1 TI=8 TF=28 NumExp=2 TI2=9 NumExp2=1 Meson=s_l yrange='0.225:0.245' FitTwoPoint.sh
+# Kaon
+p=0 TI=10 TF=26 NumExp=2 TI2=7 Meson=s_l yrange='0.187:0.197' FitTwoPoint.sh
+p=1 TI=8 TF=28 NumExp=2 TI2=9 NumExp2=1 Meson=s_l yrange='0.225:0.245' FitTwoPoint.sh
 p=2 TI=8 TF=26 NumExp=2 TI2=8 TF2=28 NumExp2=1 Meson=s_l yrange='0.250:0.280' FitTwoPoint.sh
+p=3 TI=8 TF=28 NumExp=2 TI2=6 TF2=28 NumExp2=1 Meson=s_l yrange='0.28:0.32' FitTwoPoint.sh
+#p=3 TI=8 TF=28 NumExp=2 TI2=6 TF2=22 NumExp2=1 Meson=s_l yrange='0.28:0.32' FitTwoPoint.sh #also good
+p=4 TI=10 TF=28 NumExp=2 TI2=8 TF2=28 NumExp2=1 Meson=s_l yrange='0.3:0.37' FitTwoPoint.sh
+#p=4 TI=10 TF=25 NumExp=2 TI2=8 TF2=23 NumExp2=1 Meson=s_l yrange='0.3:0.37' FitTwoPoint.sh #also good
+p=5 TI=10 TF=28 NumExp=2 TI2=12 TF2=28 NumExp2=1 Meson=s_l yrange='0.32:0.38' FitTwoPoint.sh
+p=6 TI=10 TF=28 NumExp=2 TI2=12 TF2=28 NumExp2=1 Meson=s_l yrange='0.2:0.42' FitTwoPoint.sh
+fi
